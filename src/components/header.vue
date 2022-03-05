64e77112bba222339dfc5d7cea5d023568b7c3fa
TopDevOps7 <86312703+TopDevOps7@users.noreply.github.com>
2022-03-05 16:16:05 +0900

    updaef33

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="swiper-pagination">
-    <a class="header-logo" href="/"><img src="../assets/1110z1/images/logo_1.png" /></a>
+    <a class="header-logo" href="/"
+      ><img src="../assets/1110z1/images/logo_1.png"
+    /></a>
     <!-- H5 端 header START -->
     <div class="header-nav">
       <div class="nav-text logo-m">
@@ -9,58 +11,141 @@
       <router-link class="nav-text home" to="/">HOME</router-link>
 
       <div class="nav-text">
-        <span v-bind:class="{ 'have-child-after': activeIndex === 0, 'have-child-before': activeIndex !== 0 }" @click="onNavClick(0)">CRYPTOIDS </span>
+        <span
+          v-bind:class="{
+            'have-child-after': activeIndex === 0,
+            'have-child-before': activeIndex !== 0,
+          }"
+          @click="onNavClick(0)"
+          >CRYPTOIDS
+        </span>
         <div v-bind:class="{ active: activeIndex === 0 }" class="children">
-          <router-link @click.native="onLinkClick" class="text-span" to="/story#0">Story</router-link>
-          <router-link @click.native="onLinkClick" class="text-span" to="/story#2">Breeding</router-link>
-          <router-link @click.native="onLinkClick" class="text-span" to="/story#3">Battle</router-link>
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/story#0"
+            >Story</router-link
+          >
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/story#2"
+            >Breeding</router-link
+          >
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/story#3"
+            >Battle</router-link
+          >
         </div>
       </div>
       <div class="nav-text">
-        <span v-bind:class="{ 'have-child-after': activeIndex === 1, 'have-child-before': activeIndex !== 1 }" @click="onNavClick(1)">GAME INFO </span>
+        <span
+          v-bind:class="{
+            'have-child-after': activeIndex === 1,
+            'have-child-before': activeIndex !== 1,
+          }"
+          @click="onNavClick(1)"
+          >GAME INFO
+        </span>
         <div v-bind:class="{ active: activeIndex === 1 }" class="children">
-          <router-link @click.native="onLinkClick" class="text-span" to="/sp2e#section-sp2e">sP2E</router-link>
-          <router-link @click.native="onLinkClick" class="text-span" to="/sp2e#section-tokenomics">Tokenomics</router-link>
-          <router-link @click.native="onLinkClick" class="text-span" to="/sp2e#section-nft">NFT</router-link>
-          <router-link @click.native="onLinkClick" class="text-span" to="/sp2e#section-guild">Guild & Scholarship</router-link>
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/sp2e#section-sp2e"
+            >sP2E</router-link
+          >
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/sp2e#section-tokenomics"
+            >Tokenomics</router-link
+          >
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/sp2e#section-nft"
+            >NFT</router-link
+          >
+          <router-link
+            @click.native="onLinkClick"
+            class="text-span"
+            to="/sp2e#section-guild"
+            >Guild & Scholarship</router-link
+          >
         </div>
       </div>
       <div class="nav-text">
-        <span v-bind:class="{ 'have-child-after': activeIndex === 2, 'have-child-before': activeIndex !== 2 }" @click="onNavClick(2)">GETTING STARTED</span>
+        <span
+          v-bind:class="{
+            'have-child-after': activeIndex === 2,
+            'have-child-before': activeIndex !== 2,
+          }"
+          @click="onNavClick(2)"
+          >GETTING STARTED</span
+        >
         <div v-bind:class="{ active: activeIndex === 2 }" class="children">
           <router-link class="text-span" to="/player">Player</router-link>
           <router-link class="text-span" to="/guild">Guild</router-link>
         </div>
       </div>
       <div class="nav-text">
-        <span v-bind:class="{ 'have-child-after': activeIndex === 3, 'have-child-before': activeIndex !== 3 }" @click="onNavClick(3)">MISC</span>
+        <span
+          v-bind:class="{
+            'have-child-after': activeIndex === 3,
+            'have-child-before': activeIndex !== 3,
+          }"
+          @click="onNavClick(3)"
+          >MISC</span
+        >
         <div v-bind:class="{ active: activeIndex === 3 }" class="children">
           <router-link class="text-span" to="/FAQ">FAQ</router-link>
           <div class="text-span" @click="navTextChildClick">Whitepaper</div>
           <router-link class="text-span" to="/roadmap">Roadmap</router-link>
         </div>
       </div>
-      <div class="nav-text"><a style="color: #fff;text-decoration: none;" href="https://market.cryptoids.world/">MARKETPLACE</a></div>
+      <div class="nav-text">
+        <a
+          style="color: #fff; text-decoration: none"
+          href="https://market.cryptoids.world/"
+          >MARKETPLACE</a
+        >
+      </div>
       <!--<div class="nav-btn">-->
       <!--<button @click="headerPlayClick">&nbsp;</button>-->
       <!--</div>-->
       <div class="nav-text share">
-        <a href="https://bit.ly/3GXxiJd"><img src="../assets/1110z1/images/Follow-us-10.png" /></a>
-        <a href="https://bit.ly/3IGaMWG"><img src="../assets/1110z1/images/Follow-us-7.png" /></a>
+        <a href="https://bit.ly/3GXxiJd"
+          ><img src="../assets/1110z1/images/Follow-us-10.png"
+        /></a>
+        <a href="https://bit.ly/3IGaMWG"
+          ><img src="../assets/1110z1/images/Follow-us-7.png"
+        /></a>
       </div>
       <div class="contract">
         <div>
           <span class="address">$CGC Contract Address</span>
           <div>
             <span class="code">0xF35aee66AB0D099aF233c1ab23E5f2138B0ED15B</span>
-            <button class="copy" @click="copyCode('.copy', '0xF35aee66AB0D099aF233c1ab23E5f2138B0ED15B')"></button>
+            <button
+              class="copy"
+              @click="
+                copyCode('.copy', '0xF35aee66AB0D099aF233c1ab23E5f2138B0ED15B')
+              "
+            ></button>
           </div>
         </div>
         <div>
           <span class="address">$CAC Contract Address</span>
           <div>
             <span class="code">0x5B7D8a53E63F1817B68d40DC997cB7394dB0ff1a</span>
-            <button class="copy" @click="copyCode('.copy', '0x5B7D8a53E63F1817B68d40DC997cB7394dB0ff1a')"></button>
+            <button
+              class="copy"
+              @click="
+                copyCode('.copy', '0x5B7D8a53E63F1817B68d40DC997cB7394dB0ff1a')
+              "
+            ></button>
           </div>
         </div>
       </div>
@@ -107,24 +192,33 @@
             <a href="#">MISC</a>
             <ul>
               <li><a href="/FAQ.html">FAQ</a></li>
-              <li><a href="https://cryptoids.gitbook.io/whitepaper/cryptoids/overview">Whitepaper</a></li>
               <li><a href="/Roadmap.html">Roadmap</a></li>
             </ul>
           </li>
-          <li><a href="#" data-to="marketURL" target="_blank">MARKETPLACE</a></li>
+          <li>
+            <a href="#" data-to="marketURL" target="_blank">MARKETPLACE</a>
+          </li>
+          <li>
+            <a
+              href="https://cryptoids.gitbook.io/whitepaper/cryptoids/overview"
+              data-to="marketURL"
+              target="_blank"
+              >WHITEPAPER</a
+            >
+          </li>
         </ul>
       </nav>
     </div>
     <!-- PC 端 header END -->
     <div class="header-play-logo">
       <!-- <div style="display: flex" onclick="alert('1header play logo')"> -->
-        <a data-to="playURL">
-      <div style="display: flex;cursor:pointer;">
-        <div class="logo-p">P</div>
-        <div class="logo-l">L</div>
-        <div class="logo-a">A</div>
-        <div class="logo-y">Y</div>
-      </div>
+      <a data-to="playURL">
+        <div style="display: flex; cursor: pointer">
+          <div class="logo-p">P</div>
+          <div class="logo-l">L</div>
+          <div class="logo-a">A</div>
+          <div class="logo-y">Y</div>
+        </div>
       </a>
 
       <!-- <ul class="online-ul">
@@ -169,7 +263,10 @@ export default {
     onLinkClick: function (index) {
       $(".header-nav").removeClass("full");
       if (location.hash.length > 1) {
-        document.querySelector("#contents").scrollTo({ top: $(location.hash).position().top, behavior: "smooth" });
+        document.querySelector("#contents").scrollTo({
+          top: $(location.hash).position().top,
+          behavior: "smooth",
+        });
       }
     },
     onNavClick: function (index) {
@@ -188,14 +285,14 @@ export default {
     },
     copyCode: function (id, text) {
       const clipboard = new Clipboard(id, { text: () => text });
-      clipboard.on("success", e => {
+      clipboard.on("success", (e) => {
         console.log(e.text);
         clipboard.destroy();
-        window.alert('Successfully copied');
+        window.alert("Successfully copied");
       });
-      clipboard.on("error", e => {
+      clipboard.on("error", (e) => {
         clipboard.destroy();
-        window.alert('Failed copied');
+        window.alert("Failed copied");
       });
     },
   },
